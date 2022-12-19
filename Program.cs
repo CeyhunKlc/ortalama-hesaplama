e685eb71935c3c790a3f59b123d6c855eb877def
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-12-19 09:21:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ortalamahesaplama
+{ 
+    class program
+    { 
+        static void Main(string[]args)
+        { 
+            double sinav1,sinav2,sinav3,ortalama;
+            Console.WriteLine("1.sınav notunu giriniz:");
+            sinav1=Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("2.sınav notunu giriniz:");
+            sinav2=Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("3.sınav notunu giriniz:");
+            sinav3=Convert.ToDouble(Console.ReadLine());
+            ortalama= (sinav1+sinav2+sinav3)/3;
+            Console.WriteLine(ortalama);
+        }
+    }
+}
